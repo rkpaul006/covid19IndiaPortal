2e288d13e2c8d56c9481123f800b20ce947ae196
rkpaul006 <keerthipaul20@gmail.com>
2022-12-10 15:03:29 +0000

    cases solved with spelling mistakes

--- app.js
+++ app.js
@@ -74,7 +74,7 @@ app.post("/login/", async (request, response) => {
   const dbUser = await db.get(userQuery);
   if (dbUser === undefined) {
     response.status(400);
-    response.send("invalid user");
+    response.send("Invalid user");
   } else {
     const isPasswordMatched = await bcrypt.compare(password, dbUser.password);
     if (isPasswordMatched === true) {
@@ -85,7 +85,7 @@ app.post("/login/", async (request, response) => {
       response.send({ jwtToken });
     } else {
       response.status(400);
-      response.send("Invalid Password");
+      response.send("Invalid password");
     }
   }
 });
